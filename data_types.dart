62f3fb6ed79984c70f19236734b03fff9c5a11c2
aysegulyilmaz5 <83874900+aysegulyilmaz5@users.noreply.github.com>
2022-08-11 11:41:15 +0300

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,24 @@
+void main(List<String> args){
+  int age = 34;
+  double average = 5.6;
+  num year = 1988;
+
+  print(age);
+  print(average);
+  print(year);
+
+  String name = "Aysegul";
+
+  print(name);
+  print(name + "'s favourite colors");
+  print(name + "'s age");
+
+  bool isHe = true;
+  print(isHe);
+
+  var school = "ege";
+  var schoolNumber = 1953;
+  var hasCat = true;
+
+  print(schoolNumber);
+}
